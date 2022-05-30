91039406786dc1b9e593d7734c57b6b37609862d
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2022-05-31 02:20:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMuchGrandmother;
+            int hour = 60;
+            int minutes = 10;
+            int hoursInHospital;
+            int minutesInHospital;
+
+            Console.WriteLine("Количество старушек перед вами");
+            howMuchGrandmother = Convert.ToInt32(Console.ReadLine());
+             
+            minutes = howMuchGrandmother * minutes;
+            hoursInHospital = minutes / hour;
+            minutesInHospital = minutes - hour * hoursInHospital;
+
+            Console.WriteLine("Вы должны отстоять в очереди: {0} часа, {1} минут", hoursInHospital, minutesInHospital); 
+
+        }
+    }
+}
